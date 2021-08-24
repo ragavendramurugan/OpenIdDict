d9eff8f2a28076ff17f60b6f9f7260cbb2fdef68
ragavendramurugan <54973093+ragavendramurugan@users.noreply.github.com>
2021-08-24 10:26:32 +0530

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace OpenIdDict.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> _logger; 
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
